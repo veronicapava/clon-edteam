e6916f18b82aa4bcd45d767605ebb7144a43b499
vero <vero@gmail.com>
2021-08-11 18:44:19 -0500

    Se agrega header d ela app

---
+++ src/components/Organisms/Header.jsx
@@ -0,0 +1,38 @@
+import { createRef } from "react";
+import { NavLink } from "react-router-dom";
+
+const menu = createRef()
+const toggleMenu = () => menu.current.classList.toggle('show')
+const removeToken = () => {
+  localStorage.removeItem('token')
+  window.location = "/login"
+}
+
+const Header = () => (
+  <header className="main-header">
+    <div className="ed-grid s-grid-5 lg-grid-4">
+      <div className="s-cols-4 lg-cols-1 s-cross-center">
+        <NavLink to="/">
+          <img src="https://image.flaticon.com/icons/png/512/2490/2490396.png" alt="logoEscuela" className="main-logo" />
+        </NavLink>
+      </div>
+      <div className="s-cols-1 lg-cols-3 s-cross-center s-main-end">
+        <nav className="main-menu" ref={menu}>
+          <ul>
+            <li><NavLink exact to="/">Inicio</NavLink></li>
+            <li><NavLink to="/especialidades">Especialidades</NavLink></li>
+            <li><NavLink to="/cursos">Cursos</NavLink></li>
+            <li><NavLink to="/profesores">Profesores</NavLink></li>
+            <li><span onClick={() => removeToken()}>Cerrar sesión</span></li>
+          </ul>
+        </nav>
+        <div
+          className="main-menu-toggle to-l"
+          onClick={() => toggleMenu()}
+        ></div>
+      </div>
+    </div>
+  </header>
+)
+
+export default Header
