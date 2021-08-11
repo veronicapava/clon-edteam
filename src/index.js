0eaf026bfee1040e1b42ced29ad3854c11b92c8e
vero <vero@gmail.com>
2021-08-11 18:42:05 -0500

    Agrega provider a nuestra app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import "./styles/styles.scss"
+import { Provider } from "react-redux"
+import store from "./redux/store"
+import { getAllSpecialities } from './redux/actionCreator';
+
+
+store.dispatch(getAllSpecialities())
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
